c5d8bf73f5d33d0bda83378485b5a326d618c31c
Ruben-Bellido <bellido.ruben@opendeusto.es>
2024-04-29 15:04:42 +0200

    app v0

---
+++ .venv/app/generate_data.py
@@ -0,0 +1,65 @@
+from elasticsearch import Elasticsearch
+from datetime import datetime, timezone
+import time
+import random
+import logging.config
+
+# Deshabilitar logs de librerías externas
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': True,
+})
+# Establecer logger
+logging.basicConfig(format='%(asctime)s [%(levelname)s]   %(name)s | %(message)s', datefmt='%m/%d/%Y %H:%M:%S', encoding='utf-8', level=logging.DEBUG)
+logger = logging.getLogger('generate_data')
+
+
+# Función que genera los datos sintéticos aleatoriamente
+def generate_wind_data():
+    # Generar producción energética aleatoria
+    active_power = round(random.uniform(0, 200), 2)  # LV ActivePower (kW)
+    wind_speed = round(random.uniform(3, 25), 2)  # Wind Speed (m/s)
+    theoretical_power_curve = round(active_power * random.uniform(0.8, 1.2), 2)  # Theoretical_Power_Curve (KWh)
+    wind_direction = round(random.uniform(0, 360), 2)  # Wind Direction (°)
+
+    # Devolver el diccionario con los datos
+    return {
+        "LV ActivePower (kW)": active_power,
+        "Wind Speed (m/s)": wind_speed,
+        "Theoretical_Power_Curve (KWh)": theoretical_power_curve,
+        "Wind Direction (°)": wind_direction,
+        "DateTime": datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+    }
+
+
+# Función principal de envío de datos
+def main():
+    # Configura la conexión a Elasticsearch
+    es = Elasticsearch(hosts=["http://localhost:9200"])
+
+    # Comprobar si el clúster de Elasticsearch está abierto
+    if (es.ping()):
+        logger.info("Conectado. Iniciando indexación de datos.")
+        
+        while True:
+            # Si el clúster de Elasticsearch se cierra se finaliza el envío de datos
+            if not es.ping():
+                logger.info("El clúster de Elasticsearch se ha cerrado.")
+                break
+                
+            # Generar datos sintéticos
+            wind_data = generate_wind_data()
+
+            # Indexar los datos en Elasticsearch
+            es.index(index="wind-turbine", body=wind_data)
+            logger.debug("Datos indexados correctamente: " + str(wind_data))
+
+            # Esperar 10 segundos antes de generar nuevos datos
+            time.sleep(10)
+    else:
+        logger.warning("El clúster de Elasticsearch no está abierto.")    
+
+
+# Llamada a la función principal al iniciar el programa
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # reto-visualizacion
+
+## Pasos seguidos:
+- Planteamiento y creación de la estructura del proyecto
+- Desarrollo del Docker Compose encargado de crear los servicios de Elasticsearch y Kibana y vincular uno al otro
+- Elección del método de indexación de datos y de la temática de estos
+- Desarrollo del programa de Python encargado de indexar los datos en Elasticsearch
+- Investigación de uso de Kibana
+- Creación del dashboard con gráficos para la visualización de los datos
+
+## Instrucciones de uso:
+- Estando en el directorio app, ejecutar $ docker-compose up -d
+- Inicializar entorno virtual: $ source .venv/bin/activate
+- Iniciar Elasticsearch y Kibana: $ docker-compose up -d (desde el directorio /.venv/app)
+- Indexar datos sintéticos: $ python3 generate_data.py
+
+## Posibles vías de mejora:
+- 
+
+## Problemas / Retos encontrados:
+- Seguridad (los servicios no se vinculaban debido al https)
+
+## Alternativas posibles:
+- Bases de datos NoSQL como InfluxDB o MongoDB
+- Servicios de visualización como Grafana o Prometheus
+
+https://realpython.com/python-virtual-environments-a-primer/
+https://www.elastic.co/es/blog/getting-started-with-the-elastic-stack-and-docker-compose
+https://www.elastic.co/guide/en/kibana/current/docker.html#_remove_docker_containers
